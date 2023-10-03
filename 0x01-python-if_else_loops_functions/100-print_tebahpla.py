3e2c6b0978eea490b75ccb4ba210f00d1c7c1d23
Mafupa <ryankarera@gmail.com>
2023-10-03 05:45:37 -0700

    0x01 Task 14 pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range (90, 64, -1):
-        print("{}".format(chr(i+(32 * ((i+1) % 2)))), end='')
+for i in range(90, 64, -1):
+    print("{}".format(chr(i+(32*((i+1) % 2)))), end='')
