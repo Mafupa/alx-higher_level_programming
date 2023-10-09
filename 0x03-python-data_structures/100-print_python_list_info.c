d0ce367dc2fb00508739644060a503b14bfb2073
Mafupa <ryankarera@gmail.com>
2023-10-09 15:13:51 -0700

    0x03 Task 15 test

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,7 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
 
 /**
  * print_python_list_info - prints python list info
@@ -8,11 +9,15 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	int i = 0, size = PyList_Size(p);
+	long int i = 0, size = PyList_Size(p);
 	PyListObject *obj = (PyListObject *)p;
+	PyObject *item;
 
 	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (; i < size; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %li: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
