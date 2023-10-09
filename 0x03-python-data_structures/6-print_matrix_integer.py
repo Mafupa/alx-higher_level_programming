89ade5e60fe71a0909757cf7c14c9e376c70e306
Mafupa <ryankarera@gmail.com>
2023-10-09 12:58:10 -0700

    0x03 Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}".format(i), end=' ')
-        print("")
+            print("{:d}".format(i), end=' ' if j != i[-1] else '')
