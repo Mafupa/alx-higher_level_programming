3777bb5204c40ca16810c1d228fe001f18f20192
Mafupa <ryankarera@gmail.com>
2023-10-09 12:34:19 -0700

    0x03 Task 3 attempt

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -2,7 +2,6 @@
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
 
-#my_list = [1, 2, 3, 4, 5]
-my_list = []
+my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
 
