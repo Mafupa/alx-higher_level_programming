53d9df99c9525bb15419830f91d362a460965f60
Mafupa <ryankarera@gmail.com>
2023-10-11 23:48:49 +0200

    0x04 Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or not isinstance(a_dictionary, dict):
         return None
     big = 0
     for key in a_dictionary.keys():
