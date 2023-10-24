321aa0fa6a3ca47693cad9999d9709df41c62ebd
Mafupa <ryankarera@gmail.com>
2023-10-24 10:14:50 -0700

    0x06 Task adv

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square"""
+    def __init__(self, size=0):
+        """init"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """no time"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """no time"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """no time"""
+        return self.__size ** 2
+
+    def __lt__(self, other):
+        """no time"""
+        return self.size < other.size
+
+    def __le__(self, other):
+        """no time"""
+        return self.size <= other.size
+
+    def __eq__(self, other):
+        """no time"""
+        return self.size == other.size
+
+    def __ne__(self, other):
+        """no time"""
+        return self.size != other.size
+
+    def __ge__(self, other):
+        """no time"""
+        return self.size >= other.size
+
+    def __gt__(self, other):
+        """no time"""
+        return self.size > other.size
