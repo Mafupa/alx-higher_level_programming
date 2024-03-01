353c0d8a6d9b3499b545a6f0c80cf9298a91f329
Mafupa <ryankarera@gmail.com>
2024-03-01 15:01:56 -0800

    0x11 Tasks

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Error code #0"""
+
+import sys
+from urllib import request, error
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
